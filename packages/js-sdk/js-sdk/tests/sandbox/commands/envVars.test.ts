4821c2e8c02fb36a461884bbe71fa268b04c1e34
R1Z <lastbin111504@mailfence.com>
2025-06-13 21:53:15 -0400

    Add files via upload

---
+++ packages/js-sdk/js-sdk/tests/sandbox/commands/envVars.test.ts
@@ -0,0 +1,24 @@
+import { assert, test } from 'vitest'
+
+import { sandboxTest, isDebug, template } from '../../setup.js'
+import { Sandbox } from '../../../src'
+
+sandboxTest.skipIf(isDebug)('env vars', async ({ sandbox }) => {
+  const cmd = await sandbox.commands.run('echo $FOO', { envs: { FOO: 'bar' } })
+
+  assert.equal(cmd.exitCode, 0)
+  assert.equal(cmd.stdout.trim(), 'bar')
+})
+
+test.skipIf(isDebug)('env vars on sandbox', async () => {
+  const sandbox = await Sandbox.create(template, { envs: { FOO: 'bar' } })
+
+  try {
+    const cmd = await sandbox.commands.run('echo "$FOO"')
+
+    assert.equal(cmd.exitCode, 0)
+    assert.equal(cmd.stdout.trim(), 'bar')
+  } finally {
+    await sandbox.kill()
+  }
+})
